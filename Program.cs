1aa65695a2d9e3902c00428248ba0174717c808b
marekgorskiwsb@gmail.com <gorskimarekgm@gmail.com>
2020-10-25 07:32:21 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Delegates_and_Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
